24c953ef3a7cad133c59619cf4c63a2a1600a038
aurelien0703 <aurelien.verdier@edu.ece.fr>
2022-11-09 16:54:26 +0100

    ajout gestion fichier

--- include/Gestion fichier.h
+++ include/Gestion fichier.h
@@ -1,8 +1,9 @@
-//
-// Created by aurel on 09/11/2022.
-//
+
 
 #ifndef ECE_CITY_3_E_GESTION_FICHIER_H
 #define ECE_CITY_3_E_GESTION_FICHIER_H
+intinitFichierGrille(char* nomFichier);
+int  ecritureFichierGrille(char* nomFichier,ECECITY ececity)
+int lectureFichierGrille(char * nomFichier, ECECITY ececity);
 
 #endif //ECE_CITY_3_E_GESTION_FICHIER_H
